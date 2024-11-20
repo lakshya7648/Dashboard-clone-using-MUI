45eb76e4556ebf8b2c9c055171c459ff5402f00f
Lakshya Kumar <92019850+lakshya7648@users.noreply.github.com>
2024-11-20 15:37:05 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCIcCgZn_9wx7x5ZoL8sOjpoNsWowt2M4k',
-  authDomain: 'learning-firebase-1-2c4e6.firebaseapp.com',
-  projectId: 'learning-firebase-1-2c4e6',
-  storageBucket: 'learning-firebase-1-2c4e6.appspot.com',
-  messagingSenderId: '371981219649',
-  appId: '1:371981219649:web:418affa0e288ecab9ed3c0',
-  measurementId: 'G-CL88KTH30X',
+  YOUR_FIREBASE_CONGIG
 };
 
 // Initialize Firebase
